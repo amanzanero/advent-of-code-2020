99691c17feaeefa1ffc041fd5bad511b1dfb5299
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-02 22:58:32 -0800

    day three part two

--- day3/solution.go
+++ day3/solution.go
@@ -43,6 +43,42 @@ func partOne() int {
 	return count
 }
 
+func partTwo() int {
+	parsedInput := parseInput("day3/input.txt")
+
+	slopes := [4]int{1, 3, 5, 7}
+	counts := [5]int{0, 0, 0, 0, 0}
+
+	for index, line := range parsedInput {
+		for i, slope := range slopes {
+			pos := (index * slope) % len(line)
+			isTree := line[pos] == TREE
+
+			if isTree {
+				counts[i]++
+			}
+		}
+
+		// skip down 2 slope
+		isEven := index%2 == 0
+		if isEven {
+			pos := ((index / 2) * slopes[0]) % len(line)
+			isTree := line[pos] == TREE
+
+			if isTree {
+				counts[4]++
+			}
+		}
+	}
+	ans := 1
+
+	for _, num := range counts {
+		ans *= num
+	}
+	return ans
+}
+
 func main() {
 	fmt.Println(partOne())
+	fmt.Println(partTwo())
 }
