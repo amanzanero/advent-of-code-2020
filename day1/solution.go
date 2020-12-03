8390f4f5a5a3206bd60e1806ef469280a9e27b5e
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-02 21:15:53 -0800

    day one part two

--- day1/solution.go
+++ day1/solution.go
@@ -5,7 +5,7 @@ import (
 	"github.com/amanzanero/advent-of-code-2020/lib"
 )
 
-const magicNumber = 2020
+const MAGICNUMBER = 2020
 
 func partOne(intInput []int) int {
 
@@ -13,12 +13,38 @@ func partOne(intInput []int) int {
 
 	for _, num := range intInput {
 		// found a solution
-		if compliment := magicNumber - num; seenNums[compliment] {
+		if compliment := MAGICNUMBER - num; seenNums[compliment] {
 			return num * compliment
 		}
 		// insert into seen
 		seenNums[num] = true
 	}
+	return -1
+}
+
+func partTwo(intInput []int) int {
+	// do one linear pass to get sum of two compliments
+	firstComplimentPass := make(map[int]int)
+	for _, num := range intInput {
+		compliment := MAGICNUMBER - num
+		firstComplimentPass[compliment] = num
+	}
+
+	// now we basically do part one for every element
+	for compliment, num := range firstComplimentPass {
+
+		seenNums := make(map[int]bool) // New empty set
+		for _, secondNum := range intInput {
+			// found a solution
+			if secondCompliment := compliment - secondNum; seenNums[secondCompliment] {
+				return num * secondNum * secondCompliment
+			}
+			// insert into seen
+			seenNums[secondNum] = true
+		}
+
+	}
+
 	return -1
 }
 
@@ -28,4 +54,6 @@ func main() {
 	fmt.Print("D1P1: ")
 	fmt.Println(partOne(intInput))
 
+	fmt.Print("D1P1: ")
+	fmt.Println(partTwo(intInput))
 }
