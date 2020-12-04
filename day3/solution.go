cb102abc88fc67fa8559e5d04373f3cc2a295ace
Andrew Manzanero <manzanero.andrew@gmail.com>
2020-12-03 22:41:32 -0800

    implement concurrency

--- day3/solution.go
+++ day3/solution.go
@@ -6,9 +6,15 @@ import (
 	"github.com/amanzanero/advent-of-code-2020/lib"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 const TREE uint8 = '#'
+
+type Slope struct {
+	Right int
+	Down  int
+}
 
 func parseInput(filename string) []string {
 	lines := make([]string, 0)
@@ -29,53 +35,50 @@ func parseInput(filename string) []string {
 }
 
 func partOne() int {
-	count := 0
 	parsedInput := parseInput("day3/input.txt")
 
-	for index, line := range parsedInput {
-		pos := (index * 3) % len(line)
-		isTree := line[pos] == TREE
-		if isTree {
-			count++
-		}
-	}
+	slope := Slope{3, 1}
 
-	return count
+	return countTrees(&slope, parsedInput)
 }
 
 func partTwo() int {
 	parsedInput := parseInput("day3/input.txt")
-
-	slopes := [4]int{1, 3, 5, 7}
 	counts := [5]int{0, 0, 0, 0, 0}
-
-	for index, line := range parsedInput {
-		for i, slope := range slopes {
-			pos := (index * slope) % len(line)
-			isTree := line[pos] == TREE
-
-			if isTree {
-				counts[i]++
-			}
-		}
-
-		// skip down 2 slope
-		isEven := index%2 == 0
-		if isEven {
-			pos := ((index / 2) * slopes[0]) % len(line)
-			isTree := line[pos] == TREE
-
-			if isTree {
-				counts[4]++
-			}
-		}
+	slopes := [5]Slope{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
 	}
+	wg := sync.WaitGroup{}
+	for index, slope := range slopes {
+		wg.Add(1)
+		go func(ind int, s Slope) {
+			counts[ind] = countTrees(&s, parsedInput)
+			wg.Done()
+		}(index, slope)
+	}
+	wg.Wait()
 	ans := 1
-
 	for _, num := range counts {
 		ans *= num
 	}
 	return ans
+}
+
+func countTrees(slope *Slope, lines []string) int {
+	count := 0
+	for row, iter := 0, 0; row < len(lines); row += slope.Down {
+		line := lines[row]
+		column := (slope.Right * iter) % len(line)
+		if line[column] == TREE {
+			count++
+		}
+		iter++
+	}
+	return count
 }
 
 func main() {
